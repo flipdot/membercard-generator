7139b26fa889e330df3b2d5e96ab1d5f97a512cc
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-06-17 23:06:57 +0200

    chore(nuxt): move configuration

--- src/.config/nuxt.ts
+++ src/.config/nuxt.ts
@@ -1,7 +1,7 @@
 import { VIO_NUXT_BASE_CONFIG } from '@dargmuesli/nuxt-vio/utils/nuxt'
 import { defu } from 'defu'
 
-import { SITE_NAME, STAGING_HOST } from './utils/constants'
+import { SITE_NAME, STAGING_HOST } from '../utils/constants'
 
 export default defineNuxtConfig(
   defu(
